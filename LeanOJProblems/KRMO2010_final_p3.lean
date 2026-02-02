b7d4860aa579cc39c17be34c7129a73f42436644
ansar <azhdarov.ansar@gmail.com>
2026-02-02 13:40:54 +0100

    precedence fixed

--- LeanOJProblems/KRMO2010_final_p3.lean
+++ LeanOJProblems/KRMO2010_final_p3.lean
@@ -9,8 +9,8 @@ def three_jump (L : Finset (ℕ × ℕ)) (i j : ℕ) : Prop :=
     ∃ k₁ k₂, (i, k₁) ∈ L ∧ (k₁, k₂) ∈ L ∧ (k₂, j) ∈ L
 
 theorem solution (n : ℕ) (hn : n ≥ 2) : ∃ L : Finset (ℕ × ℕ),
-    ∀ e ∈ L, is_link n e ∧
-    ∀ i ∈ Finset.Icc 1 (n - 1), (i, i + 1) ∈ L ∧
-    L.card ≤ n - 1 + 3 * (n - 1) * (Real.logb 2 (Real.logb 2 n)) ∧
-    ∀ i ∈ Finset.Icc 1 n, ∀ j ∈ Finset.Icc 1 n, i < j →
-      one_jump L i j ∨ two_jump L i j ∨ three_jump L i j := sorry
+    (∀ e ∈ L, is_link n e) ∧
+    (∀ i ∈ Finset.Icc 1 (n - 1), (i, i + 1) ∈ L) ∧
+    (L.card ≤ n - 1 + 3 * (n - 1) * (Real.logb 2 (Real.logb 2 n))) ∧
+    (∀ i ∈ Finset.Icc 1 n, ∀ j ∈ Finset.Icc 1 n, i < j →
+      one_jump L i j ∨ two_jump L i j ∨ three_jump L i j) := sorry
