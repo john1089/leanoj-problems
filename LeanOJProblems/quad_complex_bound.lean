bcbb3bfff066d48a8998903ce05d1b8aa583cba9
ansar <azhdarov.ansar@gmail.com>
2026-02-04 18:01:42 +0100

    formatting

--- LeanOJProblems/quad_complex_bound.lean
+++ LeanOJProblems/quad_complex_bound.lean
@@ -1,7 +1,8 @@
 import Mathlib.Analysis.Complex.Norm
 
+def S : Set ℝ :=
+    { t | ∃ a b c : ℂ, (∀ z : ℂ, ‖z‖ ≤ 1 → ‖a * z ^ 2 + b * z + c‖ ≤ 1) ∧ (t = ‖b * c‖) }
+
 def answer : ℝ := sorry
 
-def S : Set ℝ := { t | ∃ a b c : ℂ, (∀ z : ℂ, ‖z‖ ≤ 1 → ‖a * z ^ 2 + b * z + c‖ ≤ 1) ∧ (t = ‖b * c‖) }
-
-theorem solution : IsGreatest S answer := by sorry
+theorem solution : IsGreatest S answer := sorry
