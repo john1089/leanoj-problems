82b60011b6522c55b576e5fa8182e4a14dcfaf32
ansar <azhdarov.ansar@gmail.com>
2026-02-06 23:53:20 +0100

    added problem

---
+++ LeanOJProblems/Kazakhstan2025_District_Grade10_P1.lean
@@ -0,0 +1,4 @@
+import Mathlib.Data.Real.Basic
+
+theorem solution (a b : ℝ) (ha : 0 ≤ a) (hb : 0 ≤ b) (h : a + b = 1) :
+    (a ^ 2 + b ^ 2) / 2 ≤ a ^ 3 + b ^ 3 ∧ a ^ 3 + b ^ 3 ≤ a ^ 2 + b ^ 2 := sorry
