c4c5bab13b82debd889aa311e54e861cc1f335b1
Mihail Abramov <109156002+Michaillus@users.noreply.github.com>
2026-02-07 00:16:57 +0100

    Latvian National Olympiad 2025, 10 grade, problem 1 (#16)
    * Latvian National Olympiad 2025, 10 grade, problem 1
    
    * Fixed empty lines
    
    * updated imports
    
    ---------
    
    Co-authored-by: Ansar Azhdarov <78869764+raja-19@users.noreply.github.com>

---
+++ LeanOJProblems/Latvia2025_G10_P1.lean
@@ -0,0 +1,6 @@
+import Mathlib.Algebra.Group.Int.Defs
+import Mathlib.Algebra.Order.Group.Unbundled.Abs
+
+def answer : Prop := sorry
+
+theorem solution : (∃ a b c d : ℤ, |a - b| + |b - c| + |c - d| + |d - a| = 2025) ↔ answer := sorry 
