7428a1c6efeb0e7b39f06d76dfae15978b64f98d
ansar <azhdarov.ansar@gmail.com>
2026-02-07 18:11:02 +0100

    minor formatting

--- LeanOJProblems/fib_div.lean
+++ LeanOJProblems/fib_div.lean
@@ -4,4 +4,5 @@ import Mathlib.Data.Nat.Factorial.Basic
 import Mathlib.Order.Interval.Finset.Nat
 import Mathlib.Data.Nat.Prime.Defs
 
-theorem solution (p : ℕ) (hp1 : Nat.Prime p) (hp2 : Odd p) : p ∣ ∑ n ∈ Finset.Icc 1 (p - 1), (Nat.fib n) * (p - 1).factorial / n := sorry
+theorem solution (p : ℕ) (hp1 : Nat.Prime p) (hp2 : Odd p) :
+    p ∣ ∑ n ∈ Finset.Icc 1 (p - 1), (Nat.fib n) * (p - 1).factorial / n := sorry
