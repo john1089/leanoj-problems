045cf4dd08d1104705c5bccfd42d5dc25df51f2f
john1089 <76211484+john1089@users.noreply.github.com>
2026-02-08 20:01:43 +0800

    add the graph inequality (#18)

---
+++ LeanOJProblems/graph_ineq.lean
@@ -0,0 +1,5 @@
+import Mathlib.Combinatorics.SimpleGraph.Clique
+import Mathlib.Data.NNReal.Defs
+
+theorem solution {V : Type u} [Fintype V] [DecidableEq V] {G : SimpleGraph V} [DecidableRel G.Adj] (x : V → NNReal) :
+  ∑ e ∈ G.edgeFinset, (e.map x).mul ≤ (G.cliqueNum - 1 : ℕ) / (2 * G.cliqueNum) * (∑ v : V, x v)^2 := sorry
