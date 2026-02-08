1b3242415c2395438367661b9e46a2c96955793a
ansar <azhdarov.ansar@gmail.com>
2026-02-08 13:08:21 +0100

    minor formatting

--- LeanOJProblems/graph_ineq.lean
+++ LeanOJProblems/graph_ineq.lean
@@ -1,5 +1,6 @@
 import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Data.NNReal.Defs
 
-theorem solution {V : Type u} [Fintype V] [DecidableEq V] {G : SimpleGraph V} [DecidableRel G.Adj] (x : V → NNReal) :
-  ∑ e ∈ G.edgeFinset, (e.map x).mul ≤ (G.cliqueNum - 1 : ℕ) / (2 * G.cliqueNum) * (∑ v : V, x v)^2 := sorry
+theorem solution {V : Type u} [Fintype V] [DecidableEq V]
+    {G : SimpleGraph V} [DecidableRel G.Adj] (x : V → NNReal) : ∑ e ∈ G.edgeFinset, (e.map x).mul ≤
+    (G.cliqueNum - 1 : ℕ) / (2 * G.cliqueNum) * (∑ v : V, x v) ^ 2 := sorry
