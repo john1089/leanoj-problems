438f3d1c0c6b6dc600169b70923a560325a302b9
yhx-12243 <yhx12243@gmail.com>
2026-02-24 10:46:20 +0800

    An (MO-style) quadratic residue exercise problem (#23)

---
+++ LeanOJProblems/quadratic_residue_exercise1.lean
@@ -0,0 +1,6 @@
+import Mathlib.Data.Nat.Prime.Defs
+import Mathlib.Data.ZMod.Defs
+
+def answer : Set ℕ := sorry
+
+theorem solution : { p : ℕ | p.Prime ∧ ∀ a, 0 < a → a < p → IsSquare (a : ZMod p) → ∀ b, b ∣ a → IsSquare (b : ZMod p) } = answer := sorry
